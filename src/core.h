410ba6993a295c37da9e21d1039f3d289a1db4a9
Martin Habovštiak <martin.habovstiak@gmail.com>
2014-03-31 15:37:57 +0200

    Added copyright info

--- src/core.h
+++ src/core.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2014-INFINITY Kixunil and other Progressbar members
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
